f26a8a4f2e025a1aa0278b3cf572c60f5aed9c62
zhiyitianya <1564048529@qq.com>
2017-12-04 23:01:55 +0800

    feat():增加配置中心

---
+++ config_customer/src/main/java/com/test/clouds/ConfigCustomerApplication.java
@@ -0,0 +1,11 @@
+package com.test.clouds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigCustomerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ConfigCustomerApplication.class,args);
+    }
+}
---
+++ config_server/src/main/java/com/test/clouds/ConfigServerApplication.java
@@ -0,0 +1,29 @@
+package com.test.clouds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * URL与配置文件的映射关系如下：
+
+ /{application}/{profile}[/{label}]
+ /{application}-{profile}.yml
+ /{label}/{application}-{profile}.yml
+ /{application}-{profile}.properties
+ /{label}/{application}-{profile}.properties
+
+
+ 上述是消费者中 访问的url与配置文件在git中的映射关系
+
+ 上面的url会映射{application}-{profile}.properties对应的配置文件，{label}对应git上不同的分支，默认为master。
+
+ 我们可以尝试构造不同的url来访问不同的配置内容，比如：要访问config-label-test分支，didispace应用的prod环境，可以通过这个url：http://localhost:4000/didispace/prod/config-label-test
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
+}
