4100475545857d80fde5554ee8d5a23b406c0887
zhiyitianya <1564048529@qq.com>
2017-12-05 22:36:14 +0800

    feat():实现高可用的配置中心

--- config_customer/src/main/java/com/test/clouds/ConfigCustomerApplication.java
+++ config_customer/src/main/java/com/test/clouds/ConfigCustomerApplication.java
@@ -2,8 +2,10 @@ package com.test.clouds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigCustomerApplication {
     public static void main(String[] args){
         SpringApplication.run(ConfigCustomerApplication.class,args);
--- config_server/src/main/java/com/test/clouds/ConfigServerApplication.java
+++ config_server/src/main/java/com/test/clouds/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package com.test.clouds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -22,6 +23,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication {
     public static void main(String[] args){
         SpringApplication.run(ConfigServerApplication.class,args);
